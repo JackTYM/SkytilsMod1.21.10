5782773256902262cc5227c8d9527ff2cc0b9553
sychic <47618543+Sychic@users.noreply.github.com>
2026-02-01 08:46:46 -0500

    build: add ktx serialization, elementa, and uc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
-    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.egt.defaults)
     alias(libs.plugins.egt.loom)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 dependencies {
     modImplementation(libs.flk)
+    // hack to return non null dependency
+    include(implementation(libs.elementa.asProvider().get())!!)
+    include(modImplementation(libs.universalcraft.get())!!)
+    implementation(project(":events"))
 }
 
 tasks {
