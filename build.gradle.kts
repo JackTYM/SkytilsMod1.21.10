a3c67d2f4b4bb39de45ade842bdfe2bbab4c0dfa
sychic <47618543+Sychic@users.noreply.github.com>
2026-02-01 11:34:49 -0500

    build: add layoutdsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,20 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.egt.defaults)
     alias(libs.plugins.egt.loom)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.shadow)
 }
+
+val relocated: Configuration by configurations.creating
 
 dependencies {
     modImplementation(libs.flk)
     // hack to return non null dependency
     include(implementation(libs.elementa.asProvider().get())!!)
+    relocated(implementation(libs.elementa.layoutdsl.get())!!)
     include(modImplementation(libs.universalcraft.get())!!)
     implementation(project(":events"))
 }
@@ -24,6 +30,15 @@ tasks {
     }
 
     remapJar {
+        archiveBaseName.set(shadowJar.flatMap(ShadowJar::getArchiveBaseName))
+        inputFile.set(shadowJar.flatMap(ShadowJar::getArchiveFile))
+    }
+
+    shadowJar {
         archiveBaseName.set("Skytils-1.21.10")
+        archiveClassifier.set("dev")
+        configurations = listOf(relocated)
+
+        relocate("gg.essential.elementa.unstable", "gg.skytils.elementa.unstable")
     }
 }
