b371de71ece77c604dda9a065bedd371c8201e67
sychic <47618543+Sychic@users.noreply.github.com>
2026-02-01 08:46:31 -0500

    build: fix essential repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         maven("https://maven.architectury.dev/")
         maven("https://maven.fabricmc.net")
         maven("https://maven.minecraftforge.net/")
-        maven("https://repo.essential.gg/repository/maven-releases/")
+        maven("https://repo.essential.gg/public")
     }
 }
 
